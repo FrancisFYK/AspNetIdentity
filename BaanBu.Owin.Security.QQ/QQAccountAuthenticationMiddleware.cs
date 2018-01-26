1b7fc0304855ebba6cc6b5962a7484ad9f6e83b9
FrancisFYK <2114009146@qq.com>
2018-01-27 01:36:35 +0800

    modify

--- BaanBu.Owin.Security.QQ/QQAccountAuthenticationMiddleware.cs
+++ BaanBu.Owin.Security.QQ/QQAccountAuthenticationMiddleware.cs
@@ -2,8 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Net.Http;
 using Microsoft.Owin;
 using Microsoft.Owin.Logging;
